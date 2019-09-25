72da7aa71698c6a1ecd1c19a1e3629fb17228c3c
Leandro Lima <leandrodsouto@gmail.com>
2019-09-25 16:06:04 -0300

    add page login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,29 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //angular
 import {MatButtonModule} from '@angular/material/button';
+import { AppRoutingModule } from './app-routing.module';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material';
+import {MatInputModule} from '@angular/material';
+import { LoginComponent} from './home/login/login.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    FormsModule, ReactiveFormsModule,MatIconModule,
+    MatSelectModule,
+    MatInputModule,
     MatButtonModule,
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatFormFieldModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
