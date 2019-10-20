c123b214e473d6f2a4c3b6823ae022f1a5ac627f
marsjunior <marcos.junio@ccc.ufcg.edu.br>
2019-10-20 18:34:33 -0300

    Modificações no cadastro

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {FormControl, Validators, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +7,19 @@ import {FormControl, Validators} from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  email = new FormControl('', [Validators.required, Validators.email]);
+  email = new FormControl('', [Validators.required, Validators.email])
+  escola = {
+    nome: String,
+    email: String,
+    sigla: String,
+    cnpj : String,
+    telefone: String,
+    diretor: String,
+    senha: String,
+    confSenha: String
+  };
+  escolaForm: FormGroup;
+
 
 getErrorMessage() {
   return this.email.hasError('required') ? 'You must enter a value' :
@@ -18,6 +30,20 @@ getErrorMessage() {
   constructor() { }
 
   ngOnInit() {
+    // this.escolaForm = new FormGroup({
+    //   nomeEscola: new FormControl( this.escola.nome , [Validators.required] ),
+    //   // sigla: new FormControl(this.escola.sigla, [Validators.required]),
+    //   // cnpj: new FormControl(this.escola.cnpj, [Validators.required]), 
+    //   // telefone: new FormControl(this.escola.telefone, [Validators.required]),
+    //   nomeDiretor: new FormControl(this.escola.diretor, [Validators.required]),
+    //   senha: new FormControl(this.escola.senha, [Validators.required]),
+    //   confSenha: new FormControl(this.escola.confSenha, [Validators.required]),
+      
+    // });
+  }
+
+  registrar(){
+    console.log(this.escola);
   }
 
 }
