72312b8d673b88e3d9880fe2f8792587f6156751
marsjunior <marcos.junio@ccc.ufcg.edu.br>
2019-10-29 16:11:21 -0300

    Melhora pagina login/registro

---
+++ src/app/services/token-service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+
+  private jwtHelper: JwtHelperService;
+
+  constructor() {
+    this.jwtHelper = new JwtHelperService();
+  }
+
+  storeToken(token: string) {
+    localStorage.setItem('token', `Bearer ${token}`);
+  }
+
+  setPrivileges(privileges: any[]) {
+    const privilegeKeys = privileges.map(privilege => privilege.key);
+    localStorage.setItem('privileges', privilegeKeys.toString());
+  }
+
+  hasPrivilege(privilege: string): boolean {
+    const privileges = localStorage.getItem('privileges');
+    if (!privileges) {
+      return false;
+    }
+    return privileges.includes(privilege);
+  }
+
+  get token() {
+    return localStorage.getItem('token');
+  }
+
+  clearToken() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('privileges');
+  }
+
+  isTokenExpired(): boolean {
+    const token = localStorage.getItem('token');
+    return this.jwtHelper.isTokenExpired(token);
+  }
+
+  isAuthenticated(): boolean {
+    const token = localStorage.getItem('token');
+    return token && !this.isTokenExpired();
+  }
+}
